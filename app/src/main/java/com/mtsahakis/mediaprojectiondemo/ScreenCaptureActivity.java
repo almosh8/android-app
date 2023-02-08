f1b6dc805abc5988f15b0e9b36470e0962c13d79
almosh8 <mr.almosh443@mail.ru>
2023-02-08 02:53:14 +0100

    save projection intent to local storage for reuse

--- app/src/main/java/com/mtsahakis/mediaprojectiondemo/ScreenCaptureActivity.java
+++ app/src/main/java/com/mtsahakis/mediaprojectiondemo/ScreenCaptureActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -12,12 +13,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.google.gson.Gson;
 import com.mtsahakis.mediaprojectiondemo.services.ScreenCaptureService;
 
 
 public class ScreenCaptureActivity extends Activity {
 
     private static final int REQUEST_CODE = 100;
+    private static final String SAVED_INTENT_KEY = "Saved intent";
 
     private static Intent savedProjectionIntent = null;
 
@@ -65,6 +68,9 @@ public class ScreenCaptureActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i("kalzak", "onActivityResult resultCode = " + resultCode);
         savedProjectionIntent = data;
+        //ProjectionIntentSaver projectionIntentSaver = new ProjectionIntentSaver(this);
+        //projectionIntentSaver.saveIntent(savedProjectionIntent);
+        //savedProjectionIntent = projectionIntentSaver.getIntent();
         if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 startScreenCaptureService();
@@ -78,17 +84,32 @@ public class ScreenCaptureActivity extends Activity {
         MediaProjectionManager mProjectionManager =
                 (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
         Log.i("kalzak", "second");
-        if(savedProjectionIntent == null)
-            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
-        else {
-            startScreenCaptureService();
-        }
+            //startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+        startScreenCaptureService();
         Log.i("kalzak", "third");
     }
 
     private void startScreenCaptureService() {
-        Intent projectionIntent = (Intent) savedProjectionIntent.clone();
+        //Intent projectionIntent = (Intent) savedProjectionIntent.clone();
+        //saveIntent(projectionIntent);
+        Intent projectionIntent = getIntent();
         startService(ScreenCaptureService.getStartIntent(this, Activity.RESULT_OK, projectionIntent));
+    }
+
+    private void saveIntent(Object object) {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        editor.putString(SAVED_INTENT_KEY, json);
+        editor.apply();
+    }
+
+    private Object mgetIntent() {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(SAVED_INTENT_KEY, null);
+        return gson.fromJson(json, Intent.class);
     }
 
     private void stopProjection() {
