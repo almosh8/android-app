201bf6ec29585c25be7006ee2a44f1ffcba91844
almosh8 <mr.almosh443@mail.ru>
2023-02-08 23:34:25 +0100

    Revert "save projection intent to local storage for reuse"
    This reverts commit f1b6dc80

--- app/src/main/java/com/mtsahakis/mediaprojectiondemo/ScreenCaptureActivity.java
+++ app/src/main/java/com/mtsahakis/mediaprojectiondemo/ScreenCaptureActivity.java
@@ -4,42 +4,54 @@ import android.app.Activity;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-import com.google.gson.Gson;
-import com.mtsahakis.mediaprojectiondemo.broadcastreceivers.BroadcastReceiversManager;
 import com.mtsahakis.mediaprojectiondemo.services.ScreenCaptureService;
-import com.mtsahakis.mediaprojectiondemo.storagemanagers.ProjectionIntentSaver;
 
 
 public class ScreenCaptureActivity extends Activity {
 
     private static final int REQUEST_CODE = 100;
-    private static final String SAVED_INTENT_KEY = "Saved intent";
+
+    private static Intent savedProjectionIntent = null;
 
     /****************************************** Activity Lifecycle methods ************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        // start projection
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startScreenCapture();
+            }
+        });
+
+        // stop projection
+        Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopProjection();
+            }
+        });
 
         if (!hasUsageStatsPermission(this)) {
-            Log.i("kalzak", "launching permission intent");
             Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
             startActivity(intent);
         }
 
-        try {
-            startScreenCapture();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        BroadcastReceiversManager.registerProjectionBroadcastReceivers(this);
     }
 
     private boolean hasUsageStatsPermission(Context context) {
@@ -49,20 +61,10 @@ public class ScreenCaptureActivity extends Activity {
         return mode == AppOpsManager.MODE_ALLOWED;
     }
 
-    private void startScreenCapture() {
-        Log.i("kalzak", "starting screen capture");
-        MediaProjectionManager mProjectionManager =
-                (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
-        //startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
-        startScreenCaptureService();
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i("kalzak", "onActivityResult resultCode = " + resultCode);
-        ProjectionIntentSaver projectionIntentSaver = new ProjectionIntentSaver(this);
-        projectionIntentSaver.saveIntent(data);
-        //savedProjectionIntent = projectionIntentSaver.getIntent();
+        savedProjectionIntent = data;
         if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 startScreenCaptureService();
@@ -71,22 +73,22 @@ public class ScreenCaptureActivity extends Activity {
     }
 
     /****************************************** UI Widget Callbacks *******************************/
-
-
-    private void startScreenCaptureService() {
-        //Intent projectionIntent = (Intent) savedProjectionIntent.clone();
-        //saveIntent(projectionIntent);
-        Intent projectionIntent = getIntent();
-        startService(ScreenCaptureService.getStartIntent(this, Activity.RESULT_OK, projectionIntent));
+    private void startScreenCapture() {
+        Log.i("kalzak", "first");
+        MediaProjectionManager mProjectionManager =
+                (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Log.i("kalzak", "second");
+        if(savedProjectionIntent == null)
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+        else {
+            startScreenCaptureService();
+        }
+        Log.i("kalzak", "third");
     }
 
-
-
-    private Object mgetIntent() {
-        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = sharedPreferences.getString(SAVED_INTENT_KEY, null);
-        return gson.fromJson(json, Intent.class);
+    private void startScreenCaptureService() {
+        Intent projectionIntent = (Intent) savedProjectionIntent.clone();
+        startService(ScreenCaptureService.getStartIntent(this, Activity.RESULT_OK, projectionIntent));
     }
 
     private void stopProjection() {
